942a92b2179dbc2709b34aa7e18f08f07fd71084
samstudy <samat.study@gmail.com>
2018-01-25 22:07:50 +0600

    The main function divided as 2 function for simp. purpose

--- modified_diff.py
+++ modified_diff.py
@@ -10,33 +10,42 @@ import difflib
 import string
 
 
-def textDiff(a, b):
+def isTag(x):
+    return x[0] == "<" and x[-1] == ">"
+
+
+def get_target_html(tag, a, b):
     out = []
-    """Takes in strings a and b and returns a human-readable HTML diff."""
-    # autojunk can cause malformed HTML, but also speeds up processing.
-    matcher = difflib.SequenceMatcher(None, a, b, autojunk=False)
-    for tag in matcher.get_opcodes():
-        if tag[0] == "replace":
-            # @@ need to do something more complicated here
-            # call textDiff but not for html, but for some html... ugh
-            # gonna cop-out for now
+    if tag[0] == "replace":
             out.append('< del class="diff modified">'+''.join(a[e[1]:e[2]]) +
                        '</del><ins class="diff modified">' +
                        ''.join(b[tag[3]:tag[4]])+"</ins>")
-        elif tag[0] == "delete":
+    if tag[0] == "delete":
             out.append('<span class="deleted_element">' +
                        ''.join(a[tag[1]:tag[2]]) + "</span>")
-        elif tag[0] == "insert":
+    if tag[0] == "insert":
             out.append('<span class="added_element">' +
                        ''.join(b[tag[3]:tag[4]]) + "</span>")
-        elif tag[0] == "equal":
-            if tag[1] == tag[3] and tag[2] == tag[4]:
+    else:
+        if tag[1] == tag[3] and tag[2] == tag[4]:
                 out.append(''.join(b[tag[3]:tag[4]]))
-            else:
-                out.append('<span class="moved_element">' +
-                           ''.join(b[tag[3]:tag[4]]) + "</span>")
-                out.append('<span class="deleted_element">' +
-                           ''.join(b[tag[3]:tag[4]]) + "</span>")
         else:
-            raise "Um, something's broken. I didn't expect a '" + tag[0] + "'."
+            out.append('<span class="moved_element">' +
+                       ''.join(b[tag[3]:tag[4]]) + "</span>")
+            out.append('<span class="deleted_element">' +
+                       ''.join(b[tag[3]:tag[4]]) + "</span>")
+    return ''.join(out)
+
+
+def textDiff(a, b):
+    out = []
+    """Takes in strings a and b and returns a human-readable HTML diff."""
+    try:  # autojunk can cause malformed HTML, but also speeds up processing.
+        matcher = difflib.SequenceMatcher(None, a, b, autojunk=False)
+    except TypeError:
+        matcher = difflib.SequenceMatcher(None, a, b)
+    matcher = difflib.SequenceMatcher(None, a, b, autojunk=False)
+    for tag in matcher.get_opcodes():
+        ready_html = get_target_html(tag, a, b)
+        out.append(ready_html)
     return ''.join(out)
